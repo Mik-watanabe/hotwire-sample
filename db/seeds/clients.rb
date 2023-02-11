c1e60245bed5b5cd1d504904271f756aa3c1cd3d
mecoupe <69225308+Mik-watanabe@users.noreply.github.com>
2023-02-11 23:38:42 +0900

    init & create teble, seed & add pagination

---
+++ db/seeds/clients.rb
@@ -0,0 +1,12 @@
+first_names = [  "John",  "Jane",  "Jim",  "Sally",  "Tom",  "Kim",  "Mike",  "Emily",  "David",  "Nancy",]
+
+last_names = [  "Doe",  "Smith",  "Johnson",  "Wilson",  "Brown",  "Davis",  "Clark",  "Martinez",  "Anderson",  "Taylor",]
+
+names = []
+100.times do |i|
+  names << "#{first_names[i % first_names.length]} #{last_names[i % last_names.length]}"
+end
+
+100.times do |i|
+  Client.create(name: names[i % names.length], tel: "555-555-#{format('%04d', i+1)}")
+end
