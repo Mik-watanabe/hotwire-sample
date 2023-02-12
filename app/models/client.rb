1889187583b0659472b35e6cf02c35332e58df2a
mecoupe <69225308+Mik-watanabe@users.noreply.github.com>
2023-02-13 08:10:36 +0900

    fixed by rubocop

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,6 @@
 class Client < ApplicationRecord
-  def self.ransackable_attributes(auth_object = nil)
-    ["created_at", "name", "tel"]
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[created_at name tel]
   end
   validates :name, presence: true
   validates :tel, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_11_134205) do
-  create_table "clients", force: :cascade do |t|
-    t.string "name"
-    t.string "tel"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema[7.0].define(version: 20_230_211_134_205) do
+  create_table 'clients', force: :cascade do |t|
+    t.string 'name'
+    t.string 'tel'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
